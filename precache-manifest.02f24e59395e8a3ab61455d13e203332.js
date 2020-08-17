922814bc5a6e774f3e6487f2790d66469f1e7049
Stawlie <vdowin-nikita.vdovin@yandex.ru>
2020-08-18 01:29:54 +0300

    Updates

---
+++ precache-manifest.02f24e59395e8a3ab61455d13e203332.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6da16c32f4c2f742b7d91476d7a3f0b",
+    "url": "/ore-clicker/index.html"
+  },
+  {
+    "revision": "da38a6ddd3e3d7b55929",
+    "url": "/ore-clicker/static/css/main.d42928ae.chunk.css"
+  },
+  {
+    "revision": "c128fb7e23eaa47276c8",
+    "url": "/ore-clicker/static/js/2.b269c3d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ore-clicker/static/js/2.b269c3d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da38a6ddd3e3d7b55929",
+    "url": "/ore-clicker/static/js/main.36e02e32.chunk.js"
+  },
+  {
+    "revision": "8319722477ed7c2333f2",
+    "url": "/ore-clicker/static/js/runtime-main.750997a7.js"
+  },
+  {
+    "revision": "2f51da83df5099ce5abe03fb4dba1a54",
+    "url": "/ore-clicker/static/media/1_Minecraft-Regular.2f51da83.ttf"
+  },
+  {
+    "revision": "a215ea6e21d7f468b525420c7d35412c",
+    "url": "/ore-clicker/static/media/1_Minecraft-Regular.a215ea6e.woff"
+  },
+  {
+    "revision": "e55aa7ec649a62fd4457ed08238cdcd7",
+    "url": "/ore-clicker/static/media/1_Minecraft-Regular.e55aa7ec.eot"
+  },
+  {
+    "revision": "1af4aa94c3c149f35c30b0cda2875f13",
+    "url": "/ore-clicker/static/media/Diamond.1af4aa94.png"
+  },
+  {
+    "revision": "ece908a1ea945033ba9e2314fa0c4c29",
+    "url": "/ore-clicker/static/media/Gold.ece908a1.png"
+  },
+  {
+    "revision": "d5ddb4cec5e66bb95bd8f84e26e5608d",
+    "url": "/ore-clicker/static/media/Iron.d5ddb4ce.png"
+  },
+  {
+    "revision": "16092464f69115cff8cb57712f46f1ee",
+    "url": "/ore-clicker/static/media/Lazuli.16092464.png"
+  },
+  {
+    "revision": "bc14b7f1094d25c9f984ec61c8c30218",
+    "url": "/ore-clicker/static/media/Nether.bc14b7f1.png"
+  },
+  {
+    "revision": "aecba18951649e1e936c49f9a8000505",
+    "url": "/ore-clicker/static/media/Redstone.aecba189.png"
+  },
+  {
+    "revision": "c02cb9aee31cf76f6138a27ba00dc862",
+    "url": "/ore-clicker/static/media/bg.c02cb9ae.jpg"
+  }
+]);
